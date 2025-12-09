7400d687f0dc5d580a94518430cca22a322222ea
Ethan Wolf <Emwolf16@yahoo.com>
2025-12-08 21:52:05 -0600

    Player controls

--- src/main.c
+++ src/main.c
@@ -12,6 +12,10 @@ int main(void)
 
     InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
 
+    int playerPosition = screenHeight/2 - 40;
+
+     int moveSpeed = 10;
+
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
 
@@ -22,14 +26,20 @@ int main(void)
         //----------------------------------------------------------------------------------
         // TODO: Update your variables here
         //----------------------------------------------------------------------------------
+        if(IsKeyDown(KEY_UP)){
+            playerPosition -= 2.f * moveSpeed;
 
+        }
+        if(IsKeyDown(KEY_DOWN)){
+            playerPosition += 2.0f * moveSpeed;
+        }
         // Draw
         //----------------------------------------------------------------------------------
         BeginDrawing();
 
             ClearBackground(RAYWHITE);
 
-            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+            DrawRectangle(screenWidth/2 - 40, playerPosition, 30, 150, BLACK);
 
         EndDrawing();
         //----------------------------------------------------------------------------------
